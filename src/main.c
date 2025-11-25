43fb3d9bc87e8ff010f7561e6ee69aef76f56a0e
DiegoBCA <diegobedol232005@gmail.com>
2025-11-25 14:46:12 -0600

    final

--- src/main.c
+++ src/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include "results.h" // Importante: Conecta con tu archivo results.c
+#include <ctype.h> //libreria para usar tolower, etc
+#include "results.h" // nos permite usar showresult, es decir llamar a otros archivos con funciones
 
-// Función auxiliar para hacer preguntas de Sí/No
+// Función auxiliar, sirve para recibir la entrda, s = si / n = no
 int askQuestion(const char *question) {
-    char response;
-    printf("%s (s/n): ", question);
-    scanf(" %c", &response);
-    return (tolower(response) == 's');
+    char response; //var para guardar la respuesta
+    printf("%s (s/n): ", question); //mensaje de question
+    scanf(" %c", &response); //input
+    return (tolower(response) == 's'); //retorna 1 si es true o 0 si es false (usa tolower para convertir a minusculas)
 }
 
 int main() {
@@ -17,31 +17,38 @@ int main() {
     printf("==================================================\n\n");
     printf("Responde las siguientes preguntas para encontrar tu estructura ideal.\n\n");
 
+
+
     // --- RAMA 1: ¿Relaciones complejas o lineales? ---
     // Pregunta 1
     if (askQuestion("1. ¿Los datos representan relaciones complejas tipo red (conexiones múltiples entre elementos)?")) {
-        showResult_Graph();
-        return 0;
+        showResult_Graph(); //si la respuesta es Si, recomienda grafos
+        return 0; //termina el programa si la respuesta fue si
     }
 
+    
+
+
     // --- RAMA 2: Jerarquía y Orden ---
-    // Pregunta 2
+    // Pregunta 2 (en caso de que la respuesta anterior sea no)
     if (askQuestion("2. ¿Los datos tienen una estructura jerárquica (padres/hijos) o requieren ordenamiento rápido?")) {
         
         // Pregunta 3
         if (askQuestion("3. ¿Necesitas acceder siempre al elemento de MAYOR prioridad/valor rápidamente?")) {
-            showResult_Heap();
+            showResult_Heap(); //en caso de que sea si, recomienda un heap
         } else {
             // Es un árbol de búsqueda
-            // Pregunta 4
+            // Pregunta 4 (en caso de que sea no la pregunta anterior)
             if (askQuestion("4. ¿Te preocupa que el árbol se desbalancee y degrade el rendimiento (quieres O(log n) garantizado)?")) {
-                showResult_AVL();
+                showResult_AVL(); //en caso de poner si, recomienda AVL
             } else {
-                showResult_BST();
+                showResult_BST(); //en caso de que sea no, recomienda BST
             }
         }
-        return 0;
+        return 0; //acaba el programa
     }
+
+
 
     // --- RAMA 3: Estructuras Lineales ---
     // Si llegamos aquí, es una estructura lineal
@@ -56,6 +63,9 @@ int main() {
         }
         return 0;
     }
+
+
+    
 
     // --- RAMA 4: Almacenamiento y Acceso ---
     // Pregunta 7
